f8e825e1fa1c003274ff2f66523ac2d7d97de162
Jonathan Tsai <jontsai@users.noreply.github.com>
2026-02-25 01:56:11 -0800

    chore: exclude docs/ build output from PR (will be generated on deploy)

--- docs/_next/static/chunks/3782e2ff6616a1d8.js
+++
@@ -1,6 +0,0 @@
-__turbopack_load_page_chunks__("/blog/[slug]", [
-  "static/chunks/1219072c6f5be2fb.js",
-  "static/chunks/967da689157b8964.js",
-  "static/chunks/48e5b11c4a08fc61.js",
-  "static/chunks/turbopack-12c29f61b96e5e40.js"
-])
--- docs/_next/static/chunks/5a7395b200fee875.js
+++
@@ -1,6 +0,0 @@
-__turbopack_load_page_chunks__("/case-studies/[slug]", [
-  "static/chunks/c03fb4fadf1ffb62.js",
-  "static/chunks/967da689157b8964.js",
-  "static/chunks/48e5b11c4a08fc61.js",
-  "static/chunks/turbopack-bccab5be9908ae46.js"
-])
