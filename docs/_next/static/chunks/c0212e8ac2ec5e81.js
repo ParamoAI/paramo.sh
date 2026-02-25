ae7aa32e233f53eb73a8760f210bf15fb6eb15ec
Jonathan Tsai <jontsai@users.noreply.github.com>
2026-02-25 01:21:56 -0800

    build: add NextJS static export output
    Generated 15 pages:
    - / (home)
    - /blog/ (listing)
    - /blog/[4 posts]
    - /case-studies/ (listing)
    - /case-studies/[6 studies]
    - /404
    
    Includes sitemap.xml, assets, and all static files.

---
+++ docs/_next/static/chunks/c0212e8ac2ec5e81.js
@@ -0,0 +1,7 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e12166df33af9708.js",
+  "static/chunks/967da689157b8964.js",
+  "static/chunks/48e5b11c4a08fc61.js",
+  "static/chunks/1d3749a433662855.css",
+  "static/chunks/turbopack-19ed8735d528df10.js"
+])
